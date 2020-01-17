997d84af0dde18dae0fae30a8e0bd5491dea4266
sht819175141 <25627745+sht819175141@users.noreply.github.com>
2020-01-17 16:26:19 +0800

    modify

--- gulpfile5.js
+++ gulpfile5.js
@@ -14,13 +14,13 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('dist/css/'))
 });
 //自动刷新html
-gulp.task('server',['sass','concat','uglify'],function () {
+gulp.task('server', ['sass', 'concat', 'uglify'], function () {
     //监听scss的翻译
-    gulp.watch('src/sass/*.scss',['sass'],function () {
+    gulp.watch('src/sass/*.scss', ['sass'], function () {
         console.log('sass file changed')
     });
-    gulp.watch('src/js/*',['concat']);
-    gulp.watch('dist/js/*.js',['uglify']);
+    gulp.watch('src/js/*', ['concat']);
+    gulp.watch('dist/js/*.js', ['uglify']);
 
     return gulp.src('./')
         .pipe(serve({
@@ -30,22 +30,22 @@ gulp.task('server',['sass','concat','uglify'],function () {
         }));
 });
 
-gulp.task('concat',function () {
+gulp.task('concat', function () {
     return gulp.src('src/js/*.js')
         .pipe(concat('concat.js'))
         .pipe(gulp.dest('dist/js/'))
 });
 
-gulp.task('uglify',['concat'],function () {
+gulp.task('uglify', ['concat'], function () {
     return gulp.src('dist/js/concat.js')
         .pipe(uglify())
         .pipe(rename('concat.min.js'))
         .pipe(gulp.dest('dist/js/'))
 });
 
-gulp.task('clean',function () {
+gulp.task('clean', function () {
     return gulp.src('dist/')
         .pipe(clean())
 });
 
-gulp.task('default',['clean','server']);
+gulp.task('default', ['clean', 'server']);
